6e18516b3aa561529e9a55169359baa05c146502
xiliuhk <xiliuhk@gmail.com>
2014-10-04 19:37:31 -0400

    qiliao

--- src/main/java/CasConsumer.java
+++ src/main/java/CasConsumer.java
@@ -1,4 +1,29 @@
+RawBlameHistory   
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
 
-public class CasConsumer {
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.collection.CasConsumer_ImplBase;
+import org.apache.uima.collection.base_cpm.CasObjectProcessor;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.ResourceProcessException;
+import org.apache.uima.resource.metadata.ProcessingResourceMetaData;
+import org.apache.uima.util.ProcessTrace;
+
+public class CasConsumer extends CasConsumer_ImplBase implements CasObjectProcessor {
+
+	@Override
+	public void processCas(CAS aCAS) throws ResourceProcessException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/main/java/CollectionReader.java
+++ src/main/java/CollectionReader.java
@@ -1,4 +1,37 @@
+import java.io.File;
+import java.io.IOException;
 
-public class CollectionReader {
+import org.apache.uima.cas.CAS;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.collection.CollectionReader_ImplBase;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.util.FileUtils;
+import org.apache.uima.util.Progress;
+
+public class CollectionReader extends CollectionReader_ImplBase {
+
+	@Override
+	public void getNext(CAS aCAS) throws IOException, CollectionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean hasNext() throws IOException, CollectionException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Progress[] getProgress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
